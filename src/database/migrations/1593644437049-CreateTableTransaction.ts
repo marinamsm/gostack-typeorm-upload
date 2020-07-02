a3a3d31972b7f304fa26d37dc9b5b7b007b98f12
marinamsm <marinasouza.msm@gmail.com>
2020-07-01 21:08:40 -0300

    Migration files to create tables

---
+++ src/database/migrations/1593644437049-CreateTableTransaction.ts
@@ -0,0 +1,72 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTableTransaction1593644437049
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        foreignKeys: [
+          {
+            name: 'TransactionCategory',
+            columnNames: ['category_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'categories',
+            onDelete: 'SET NULL',
+            onUpdate: 'CASCADE',
+          },
+        ],
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+            isNullable: false,
+            isPrimary: true,
+            isUnique: true,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+            enum: ['income', 'outcome'],
+            isNullable: false,
+          },
+          {
+            name: 'value',
+            type: 'real',
+            default: 0,
+            isNullable: false,
+          },
+          {
+            name: 'category_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+            isNullable: false,
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+            isNullable: false,
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('transactions', true);
+  }
+}
---
+++ src/database/migrations/1593644449014-CreateTableCategory.ts
@@ -0,0 +1,45 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTableCategory1593644449014
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+            isNullable: false,
+            isPrimary: true,
+            isUnique: true,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+            isNullable: false,
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+            isNullable: false,
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('categories', true);
+  }
+}
