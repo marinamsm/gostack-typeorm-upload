7404728e7b7e0f6d797fd56eb27721b1935c58ed
marinamsm <marinasouza.msm@gmail.com>
2020-07-04 06:16:48 -0300

    Can't create outcome bigger than income. Dele transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,12 @@
-import { getRepository } from 'typeorm';
+import { getCustomRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateCategoryService from './CreateCategoryService';
+import AppError from '../errors/AppError';
 
 interface Request {
   title: string;
-  type: 'income' | 'outcome';
+  type: 'income' | 'outcome' | string;
   value: number;
   category: string;
 }
@@ -17,7 +19,13 @@ class CreateTransactionService {
     category,
   }: Request): Promise<Transaction> {
     const categoryService = new CreateCategoryService();
-    const transactionRepo = getRepository(Transaction);
+    const transactionRepo = getCustomRepository(TransactionsRepository);
+
+    const { income } = await transactionRepo.getBalance();
+
+    if (type === 'outcome' && value > income) {
+      throw new AppError('Invalid transaction');
+    }
 
     const newCategory = await categoryService.execute({ title: category });
 
