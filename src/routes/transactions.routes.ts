7f44284f57d6ff805ca02f04ad1881eee1f1ba46
marinamsm <marinasouza.msm@gmail.com>
2020-07-04 06:17:43 -0300

    Routes

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,11 +1,14 @@
+import multer from 'multer';
 import { Router } from 'express';
 
+import uploadConfig from '../config/upload';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
+const upload = multer(uploadConfig);
 
 transactionsRouter.get('/', async (request, response) => {
   const transactionRepo = new TransactionsRepository();
@@ -36,11 +39,26 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const transactionService = new DeleteTransactionService();
+
+  await transactionService.execute(id);
+
+  return response.status(204).send();
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('transactions'),
+  async (request, response) => {
+    console.log('FILE---', request.file);
+    const transactionService = new ImportTransactionsService();
+
+    const transactions = await transactionService.execute(request.file);
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
